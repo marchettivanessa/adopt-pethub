7e34f96cc34e625f50ceee6b8bab81fdfc17fefe
Vanessa Marchetti <vanessa.marchetti@sumup.com>
2024-10-19 23:04:54 -0300

    Create tables

---
+++ migrations/000002_create_tables.down.sql
@@ -0,0 +1,17 @@
+-- Remover a tabela de feedbacks
+DROP TABLE IF EXISTS adopt_pethub.feedbacks CASCADE;
+
+-- Remover a tabela de abrigos
+DROP TABLE IF EXISTS adopt_pethub.abrigos CASCADE;
+
+-- Remover a tabela de adoções
+DROP TABLE IF EXISTS adopt_pethub.adocoes CASCADE;
+
+-- Remover a tabela de animais
+DROP TABLE IF EXISTS adopt_pethub.animais CASCADE;
+
+-- Remover a tabela de usuários
+DROP TABLE IF EXISTS adopt_pethub.usuarios CASCADE;
+
+-- Remover o schema adopt_pethub
+DROP SCHEMA IF EXISTS adopt_pethub CASCADE;
